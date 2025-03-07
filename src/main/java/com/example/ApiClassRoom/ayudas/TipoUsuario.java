4b1c0e927ef47952fa83cacdaed8032050e7774e
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-03-07 10:27:10 -0500

    Se crea tabla

---
+++ src/main/java/com/example/ApiClassRoom/ayudas/TipoUsuario.java
@@ -0,0 +1,7 @@
+package com.example.ApiClassRoom.ayudas;
+
+public enum TipoUsuario {
+    Estudiante,
+    Docente,
+    Administrador
+}
---
+++ src/main/java/com/example/ApiClassRoom/models/Estudiante.java
@@ -0,0 +1,14 @@
+package com.example.ApiClassRoom.models;
+
+import java.time.LocalDate;
+
+public class Estudiante {
+    private Integer id;
+    private Integer grado;
+    private LocalDate fechaNacimiento;
+
+
+
+
+
+}
